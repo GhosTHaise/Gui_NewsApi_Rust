56819f9961a88bb3d26a9e5ee136000ac5c48bd7
GhosTHaise <Ghostrex2@gmail.com>
2022-08-09 15:47:42 +0300

    Create first window

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,3 +1,19 @@
+use eframe::{egui::CentralPanel,epi::App,run_native, NativeOptions};
+struct Headlines;
+
+impl App for Headlines{
+    fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
+        CentralPanel::default().show(ctx, |ui|{
+            ui.label("article text");
+        });
+    }
+
+    fn name(&self) -> &str {
+        "Headlines"
+    }
+}
 fn main() -> () {
-    
+    let app = Headlines;
+    let win_options = NativeOptions::default();
+    run_native(Box::new(app),win_options);
 }
