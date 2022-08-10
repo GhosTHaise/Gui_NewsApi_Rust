6113884e2874f2c2c565df937cdc65e0acdae84f
GhosTHaise <Ghostrex2@gmail.com>
2022-08-10 12:00:44 +0300

    Add fonts to window

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,6 +1,6 @@
-use std::fmt::format;
+use std::{fmt::format, borrow::Cow};
 
-use eframe::{egui::{CentralPanel, ScrollArea},epi::App,run_native, NativeOptions};
+use eframe::{egui::{CentralPanel, ScrollArea, Vec2, FontDefinitions, FontFamily},epi::App,run_native, NativeOptions};
 struct Headlines{
     articles : Vec<NewsCardData>,
 }
@@ -15,6 +15,22 @@ impl Headlines {
             articles : Vec::from_iter(iter)
          }
     }
+    fn configure_fonts(&self,ctx: &eframe::egui::CtxRef) -> () {
+        // create font def object
+        let mut font_def = FontDefinitions::default();
+        // then we we'll load the font
+        font_def.font_data.insert("Spartan".to_string(), Cow::Borrowed(include_bytes!("../../Spartan-VariableFont_wght.ttf")));
+        // then set the sowe of different text styles
+        font_def.family_and_size.insert(eframe::egui::TextStyle::Heading,(FontFamily::Proportional,35.));
+        font_def.family_and_size.insert(eframe::egui::TextStyle::Body,(FontFamily::Proportional,20.));
+        
+        font_def.fonts_for_family.get_mut(&FontFamily::Proportional)
+                                 .unwrap()
+                                 .insert(0,"Spartan".to_string());
+        // load font using context object
+        ctx.set_fonts(font_def)   ;                      
+
+    }
 }
 
 struct NewsCardData{
@@ -24,6 +40,14 @@ struct NewsCardData{
 }
 
 impl App for Headlines{
+    fn setup(
+            &mut self,
+            ctx: &eframe::egui::CtxRef,
+            _frame: &mut eframe::epi::Frame<'_>,
+            _storage: Option<&dyn eframe::epi::Storage>,
+        ) {
+        self.configure_fonts(ctx);
+    }
     fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
         CentralPanel::default().show(ctx, |ui|{
             ScrollArea::auto_sized().show(ui, |ui|{
@@ -37,11 +61,12 @@ impl App for Headlines{
     }
 
     fn name(&self) -> &str {
-        "Headlines"
+        "Headlines by GhosT v1.0"
     }
 }
 fn main() -> () {
     let app = Headlines::new();
-    let win_options = NativeOptions::default();
+    let mut win_options = NativeOptions::default();
+    win_options.initial_window_size = Some(Vec2::new(600., 760.));
     run_native(Box::new(app),win_options);
 }
